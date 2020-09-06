0c6306ca3c8259cd6c7da7737712429158b960ae
Tia Low <lowtia@gmail.com>
2020-09-06 16:27:07 -0700

    Added env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
